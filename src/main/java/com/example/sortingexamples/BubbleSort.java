03819825683ab670b4a5ea93cbd644a9d4c43153
a.zaytseva <a.zaytseva@cian.ru>
2022-10-19 13:05:51 +0300

    improve bubble sort

--- src/main/java/com/example/sortingexamples/BubbleSort.java
+++ src/main/java/com/example/sortingexamples/BubbleSort.java
@@ -3,6 +3,16 @@ package com.example.sortingexamples;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+/*
+[1, 6, 5, 2, 10, 1]
+
+1 -> [1, 5, 2, 6, 10, 1]
+2 -> [1, 5, 2, 6, 1, 10]
+3 -> [1, 2, 5, 6, 1, 10]
+4 -> [1, 2, 5, 1, 6, 10]
+5 -> [1, 2, 1, 5, 6, 10]
+6 -> [1, 1, 2, 5, 6, 10]
+ */
 @Slf4j
 @Service("bubble")
 public class BubbleSort implements SortInterface {
@@ -23,15 +33,28 @@ public class BubbleSort implements SortInterface {
             return;
         }
 
-        for(int i = 0; i < n - 1; i++) {
+        for(int i = 0; i < n - 1; ++i) {
             if (array[i] > array[i + 1]) {
                 swap(array, i, i + 1);
+                log.info("{}", array);
             }
         }
 
         bubbleSort(array, n - 1);
 
     }
+
+//    private void bubbleSort(int[] array, int n) {
+//
+//        for(int i = 0; i < n - 1; i++) {
+//            for (int j = 0; j < n - i - 1; j++) {
+//                if (array[j] > array[j + 1]) {
+//                    swap(array, j, j + 1);
+//                }
+//            }
+//        }
+//
+//    }
 
     private void swap(int[] array, int index1, int index2) {
 
